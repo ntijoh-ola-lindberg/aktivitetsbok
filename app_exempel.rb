b171b7f57ebe3359bac7f707d5e70dda022cbf9b
Ola Lindberg <www.olalindberg.com>
2020-01-27 11:52:43 +0100

    Update app_exempel.rb

---
+++ app_exempel.rb
@@ -0,0 +1,65 @@
+require './model/anvandare.rb'
+require './model/student.rb'
+require './model/larare.rb'
+require './model/klass.rb'
+require './model/logg.rb'
+
+# Exempelkod. Pseudo-kod. Ej tänkt att köras.
+class AppController < Sinatra::Base
+
+    before do
+        @anvandare = Student.logga_in( 1, 'aoeu' )
+    end
+
+    get '/' do
+        # todo: redirect till /student om vi är 
+        #       student och /larare om vi är lärare
+    end
+
+    # Hämtar elevens loggar från databasen och visar i vyn
+    get '/student' do
+        @loggar = @anvandare.loggar
+
+        slim :student
+    end
+
+    # Läser fyra fält från vyn, 
+    # spara till databasen
+    # redirectar till studentents förstasida
+    post '/student/skriv_logg' do 
+        gjort = params['gjort'].to_s
+        lart = params['lart'].to_s
+        forstatt = params['forstatt'].to_s
+        mer = params['mer'].to_s
+
+        logg = Logg.new( gjort, lart, forstatt, mer )
+        @anvandare.skriv(logg)
+
+        redirect '/student'
+    end
+
+    # Hämtar lärarens klasser från databasen och visar i vyn
+    get '/larare' do
+        @klasser = @anvandare.klasser
+
+        slim :larare
+    end
+
+    # Läser tre fält från vyn, 
+    # skapar en ny student och en ny klass, 
+    # lägger till studenten i klassen och
+    # redirectar till lärarens förstasida
+    post '/larare/registrera_student' do
+        anvandar_namn = params['anvandarnamn'].to_s
+        losenord_text = params['losenord'].to_s
+        klass_namn = params['klass'].to_s
+
+        student = Student.new ( anvandar_namn, losenord_text )
+        @anvandare.registrera ( student )
+        klass = Klass.new ( klass_namn )
+        lägg_till ( student, klass )
+
+        redirect '/larare'
+    end
+
+end
